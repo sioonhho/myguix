0ea4a11c4c913e3e66e68c18927cb6ece8f21740
Andrew DeFilippo <sioonhho@tuta.io>
2022-12-09 15:45:22 -0600

    changed module path

--- packages/rust-chvt.scm
+++ packages/rust-chvt.scm
@@ -1,4 +1,4 @@
-(define-module (myguix packages rust-chvt)
+(define-module (packages rust-chvt)
   #:use-module (guix build-system cargo)
   #:use-module (guix download)
   ;#:use-module (guix git-download)
