3d0b40df83ba7a61cf7db918f2c9d1875d81da3c
Andrew DeFilippo <sioonhho@tuta.io>
2022-12-12 13:48:18 -0600

    fix

--- myguix/packages.scm
+++ myguix/packages.scm
@@ -1,16 +1,15 @@
 (define-module (myguix packages)
   #:use-module (gnu packages)
-  #:use-module (srfi srfi-1)
   #:export (search-patch
 	    search-patches
 	    %patch-path))
 
 (define (search-patch file-name)
-  (or (search-patch (%patch-path) file-name)
+  (or (search-path (%patch-path) file-name)
       (raise (formatted-message (G_ "~a: patch not found")
 				file-name))))
 
-(define-sysntax-rule (search-patches file-name ...)
+(define-syntax-rule (search-patches file-name ...)
   (list (search-patch file-name) ...))
 
 (define %channel-root
